142c92829c8b4e414b95c0e9eca31804cbb64165
Sargun Kohli <sargun.kohli@impactanalytics.co>
2025-06-05 23:37:51 +0530

    changes with dist file in server dir

--- server/dist/index.js
+++ server/dist/index.js
@@ -24,7 +24,7 @@ const app = (0, express_1.default)();
 app.use((0, cookie_parser_1.default)());
 // CORS configuration
 app.use((0, cors_1.default)({
-    origin: 'http://localhost:3000',
+    origin: ['http://localhost:3000', 'https://inventory-management-frontend-m0y6.onrender.com'],
     credentials: true,
 }));
 app.use((0, helmet_1.default)());
