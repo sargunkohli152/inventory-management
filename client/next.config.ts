614c506bb4b6b2954ac3695a3ec4414f6ff01590
Sargun Kohli <sargun.kohli@impactanalytics.co>
2025-06-06 16:00:23 +0530

    added extra config for frontend

--- client/next.config.ts
+++ client/next.config.ts
@@ -7,6 +7,9 @@ const nextConfig: NextConfig = {
   typescript: {
     ignoreBuildErrors: true,
   },
+  images: {
+    domains: ['inventory-management-backend.onrender.com'],
+  },
 };
 
 export default nextConfig;
